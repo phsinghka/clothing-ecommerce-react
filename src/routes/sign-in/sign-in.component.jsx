5a875fc534de6c33b85de974cc19bb32971ef4c7
Piyush Himmatsinghka <phsinghka@Piyushs-MacBook-Air.local>
2022-10-31 20:38:28 -0400

    Added Firebase Google Sign In

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,20 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentwithAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <h1>Sign In Page</h1>;
+  const logCurrentUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+    const userDocRef = await createUserDocumentwithAuth(response.user);
+  };
+  return (
+    <div>
+      <h1>Sign In Page</h1>;
+      <button onClick={logCurrentUser}>Sign In with google</button>;
+    </div>
+  );
 };
 
 export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,12 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdvKj2JMmYSqSR7ez3kYiihGtpqbM6S1E",
@@ -11,3 +19,38 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentwithAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (e) {
+      console.log("There was an error creating the User", e.message);
+    }
+  }
+
+  return userDocRef;
+};
